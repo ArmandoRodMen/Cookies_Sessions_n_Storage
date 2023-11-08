37d89db3e197229d3d487eb3d770d4c0022fccfc
Armando Rodriguez <armandorodriguezm1998@gmail.com>
2023-11-08 08:30:59 -0600

    primer commit

---
+++ src/routes/sessions.router.js
@@ -0,0 +1,68 @@
+import { Router } from "express";
+import { usersManager } from "../DAO/mongodb/managers/usersManager.js";
+
+const router = Router();
+
+router.post("/signup", async (req, res) => {
+        const { first_name, last_name, email, password, username } = req.body;
+        
+        if (!first_name || !last_name || !email || !password || !username) {
+        return res.status(400).json({ message: "Some data is missing" });
+        }
+        
+        try {
+        const createdUser = await usersManager.createOne(req.body);
+        
+        // Suponiendo que createdUser contiene el ID del usuario recién creado
+        const userId = createdUser.id; // Ajusta esto a la forma en que obtienes el ID
+    
+        // Construye la URL de redirección con el ID del usuario
+        const redirectUrl = `/profile/${userId}`;
+        
+        // Redirige al usuario a la ruta /profile/:idUser
+        res.redirect(redirectUrl);
+        console.log(redirectUrl);
+        } catch (error) {
+        res.status(500).json({ error });
+        }
+    });
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "Some data is missing" });
+    }
+
+    try {
+        const user = await usersManager.findByEmail(email);
+        if (!user) {
+            return res.redirect("/signup");
+        }
+        const isPasswordValid = password === user.password;
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Password is not valid" });
+        }
+        
+        // Obten el ID del usuario autenticado
+        const userId = user.id;
+
+        const sessionInfo = (email === "adminCoder@coder.com" && password === "adminCod3r123")
+            ? { email, first_name: user.first_name, isAdmin: true }
+            : { email, first_name: user.first_name, isAdmin: false };
+        req.session.user = sessionInfo;
+
+        // Redirige al usuario a la página de perfil con el ID del usuario
+        res.redirect(`/profile/${userId}`);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+});
+
+router.get("/signout", (req, res)=>{
+    req.session.destroy(()=>{
+        res.redirect("/login");
+    });
+});
+
+export default router;
---
+++ src/routes/views.router.js
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { messagesManager } from "../DAO/mongodb/managers/messagesManager.js";
+import { usersManager } from "../DAO/mongodb/managers/usersManager.js";
+import { productsManager } from "../DAO/mongodb/managers/productsManager.js";
+import { cartsManager } from "../DAO/mongodb/managers/cartsManager.js";
+
+const router = Router();
+
+router.get("/chat", async (req, res) => {
+  const messages = await messagesManager.findAll();
+  const {username } = username;
+  res.render("chat", { messages, username });
+});
+
+router.get("/products", async (req, res) => {
+  const products = await productsManager.findAggregation(req.query);
+  res.render("products", { products: products });
+});
+
+router.get("/cart/:idCart", async (req, res) => {
+  const {idCart} = req.params;
+  const cartProducts = await cartsManager.findProductsInCart(idCart);
+  res.render("cart", {idCart, products:cartProducts} );
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/profile/:idUser", async (req, res) => {
+  const { idUser } = req.params;
+  const user = await usersManager.findById(idUser);
+  const products = await productsManager.findAll();
+  const { first_name, last_name, username } = user;
+  res.render("profile", { first_name, last_name, username, products });
+});
+
+export default router;
